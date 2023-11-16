d33bc8cdcef754818c476a8fc8f80cdf961b4d76
DevDK12 <devdk2208@gmail.com>
2023-11-17 01:51:45 +0530

    Created Single User View && Implemented its Fetch

--- app/server-actions/data.js
+++ app/server-actions/data.js
@@ -19,3 +19,25 @@ export const fetchUsers = async () => {
     }
 };
 
+
+
+
+
+
+
+
+
+
+export const fetchUser = async (id) => {
+    try {
+        connectToDB();
+        const user = await User.findById(id);
+        return user;
+    } 
+    catch (err) {
+        console.log(err);
+        throw new Error("Failed to fetch user!");
+    }
+};
+
+
