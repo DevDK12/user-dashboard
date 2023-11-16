f57b6a8ccc8164ae8d669c5e229fef3cfd59aa7d
DevDK12 <devdk2208@gmail.com>
2023-11-17 01:13:26 +0530

    Implemented Mongodb Schema, Add User feature to db

--- app/(pages)/dashboard/users/add/page.jsx
+++ app/(pages)/dashboard/users/add/page.jsx
@@ -1,11 +1,11 @@
 import styles from "@/app/components/dashboard/users/addUser/addUser.module.css";
-
+import { addUser } from "@/app/server-actions/actions";
 
 
 const AddUserPage = () => {
     return (
         <div className={styles.container}>
-            <form className={styles.form}>
+            <form action={addUser} className={styles.form}>
                 <input type="text" placeholder="username" name="username" required />
                 <input type="email" placeholder="email" name="email" required />
                 <input
